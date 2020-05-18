dad9d7bbc46294981abf29057b25c4c04af23a8b
odinaka-joy <odinakajoy909@gmail.com>
2020-05-18 05:52:10 +0100

    add build folder

---
+++ build/precache-manifest.a28b0ad29a048344cb480c9ddaaf2b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c63834ceb804df479efb62d0966f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "274a5dade647469f941a",
+    "url": "/static/css/main.d8ecb489.chunk.css"
+  },
+  {
+    "revision": "549db0c8f3858f7813df",
+    "url": "/static/js/2.0b1dd946.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0b1dd946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274a5dade647469f941a",
+    "url": "/static/js/main.fa42c408.chunk.js"
+  },
+  {
+    "revision": "e3e162a2466e905bd7b9",
+    "url": "/static/js/runtime-main.bdc5f5b0.js"
+  }
+]);
